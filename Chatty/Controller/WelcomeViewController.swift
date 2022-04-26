635ad4ca8db5b294eba76aa704b7ba16a47a5066
NikLev7 <spikywu7@gmail.com>
2022-04-26 21:31:02 +0300

    Initial Commit

---
+++ Chatty/Controller/WelcomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chatty
+//
+//  Created by Spiky WU7 on 26.04.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Setup
+    }
+
+
+}
+
