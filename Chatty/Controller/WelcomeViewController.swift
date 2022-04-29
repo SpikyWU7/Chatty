9bce9e3359a869dc79764a29ed1ae47d6ca8197f
NikLev7 <spikywu7@gmail.com>
2022-04-29 13:12:21 +0300

    Pod CLTypingLabel
    Added pod

--- Chatty/Controller/WelcomeViewController.swift
+++ Chatty/Controller/WelcomeViewController.swift
@@ -6,16 +6,38 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //Setup
+        
+        titleLabel.text = "💬CHATTY💭"
+
     }
 
 
 }
 
+
+
+
+
+
+
+
+
+
+
+//        titleLabel.text = ""
+//        var characterIndex = 0
+//        let titleText = "💬CHATTY💭"
+//        for letter in titleText {
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * Double(characterIndex), repeats: false) { (timer) in
+//                self.titleLabel.text?.append(letter)
+//            }
+//            characterIndex += 1
+//        }
---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '13.0'
+
+target 'Chatty' do
+  use_frameworks!
+
+  # Pods for Chatty
+
+    pod 'CLTypingLabel', '~> 0.4.0'
+
+end
